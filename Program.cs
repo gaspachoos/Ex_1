3cf265f01d100d2a6b85f3284b7f61a385f1858c
Garik <gar.minasyan.97@mail.ru>
2024-02-03 10:35:33 +0400

    Добавили рекурсивную функцию

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿int FindDigits(int m, int n)
+{
+    int start = m;
+    int finish = n;
+    if (m > n)
+    {
+        start = n;
+        finish = m;
+        Console.WriteLine("Вы ввели стартовое значение больше чем финальное,программа поменяла их местами: ");
+    }
+
+    if (start == finish)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Рекурсия остановилась достигнув финального значения {finish}");
+        return start;
+    }
+    Console.Write($"{start} ");
+    FindDigits(start + 1, finish);
+    return start;
+
+}
